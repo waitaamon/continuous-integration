c0bb060c952ee1db18a8bee18be1e2f694e730fb
waitaamon <waitaamon@gmail.com>
2024-10-24 14:40:14 +0300

    wip

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -21,6 +21,7 @@ class CreateNewUser implements CreatesNewUsers
      */
     public function create(array $input): User
     {
+        //this is a random comment
         Validator::make($input, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
