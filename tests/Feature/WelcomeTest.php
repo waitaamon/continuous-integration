22c835fca2c1f7608c3b9e760445787a8c32e888
waitaamon <waitaamon@gmail.com>
2024-10-24 18:17:13 +0300

    wip

--- tests/Feature/WelcomeTest.php
+++ tests/Feature/WelcomeTest.php
@@ -4,7 +4,7 @@ use App\Models\User;
 
 it('provides users in a random paginated order', function () {
 
-    $users = User::factory()->count(4)->create();
+    $users = User::factory()->count(5)->create();
 
     $users = collect($this->get('/')->viewData('users')->items())
         ->merge($this->get('/?page=2')->viewData('users')->items());
